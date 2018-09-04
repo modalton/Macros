ec6cccf3e79f87938978cfcbf9ba682fa430affc
Michael Dalton <modintexas@gmail.com>
2018-09-03 17:46:24 -0700

    Basic gensym use

---
+++ gensym.lisp
@@ -0,0 +1,27 @@
+
+(defmacro bad-for ((var start stop) &body body)
+  "Limit not necessary but illustrative of capture"
+  `(do ((,var ,start (1+ ,var))
+        (limit ,stop))
+       ((> ,var limit))
+     ,@body))
+
+
+
+(defmacro for ((var start stop) &body body)
+  (let ((limit (gensym)))
+    `(do ((,var ,start (1+ ,var))
+         (,limit ,stop))
+        ((> ,var ,limit))
+      ,@body)))
+
+
+
+(defun run ()
+  "Do macroexpands correctly and doesn't capture. Book doesn't specify how it's vulnerable inside that do although you can see clearly free variable."
+  (let ((limit 7))
+    (bad-for (x 0 10)
+      (print x))
+    (for (x 0 10)
+      (print x))
+    (print limit)))
